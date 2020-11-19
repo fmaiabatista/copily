8f160422a17fdc22cad99b23bbe6a2f253ac45ee
Felipe Maia <felipe.cruz@radixeng.com>
2020-11-19 09:19:41 -0300

    implemented first page

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './Form.css'
+import arrowRight from './assets/img/arrow-right.svg'
 
 const Form: React.FunctionComponent = () => {
+  const handleClick = () => {
+    // openRoom()
+  }
+
   return (
     <div className="Form">
-      <div>input</div>
-      <div>button</div>
+      <div>
+        <input
+          type="text"
+          name="room-name"
+          className="input-room"
+          placeholder="enter room name"
+        />
+      </div>
+      <div>
+        <button type="button" onClick={handleClick}>
+          <img className="arrow-right" src={arrowRight} alt="arrow-right" />
+        </button>
+      </div>
     </div>
   )
 }
